fbadf05e4e2359f8124433d9f0635b0c942ab8d0
sherwin <140298914+2smill@users.noreply.github.com>
2023-09-29 18:26:35 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CardComponent from './components/CardComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <CardComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CardComponent.js
@@ -0,0 +1,174 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import { Container, Card, Button } from 'react-bootstrap';
+import './CardComponent.css'; 
+
+const CardComponent = () => {
+  return (
+
+    <div id="home" class="container-fluid bodyBg mt-0 mb-5 p-5">
+
+    <div class="container-fluid mrgnt">
+
+      <div class="container-fluid mt-md-5">
+
+        <div class="row">
+
+          <div class="col-md-6 ml-md-5 home-header">
+
+            <h1>
+
+              The Growing Environmental Risks of E-Waste
+
+            </h1>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <div class="container-fluid">
+      <div class="container-fluid mt-md-5">
+        <div class="row">
+          <div class="col-md-6 ml-md-5 home-paragraph">
+            <p>
+            E-waste, electronic waste, e-scrap and end-of-life electronics are terms often used to describe used electronics 
+            that are nearing the end of their useful life, and are discarded, donated or given to a recycler. The UN defines 
+            e-waste as any discarded products with a battery or plug, and features toxic and hazardous substances such as mercury, 
+            that can pose severe risk to human and environmental health.
+            </p>
+            <a href="https://www.genevaenvironmentnetwork.org/resources/updates/the-growing-environmental-risks-of-e-waste/">Read More</a>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+      <Container>
+        
+        <div className="card-group vgr-cards">
+          <Card>
+            <div className="card-img-body">
+              <Card.Img src="https://www.tes-amm.com/hubfs/03.%20Website%20Pages/01.%20Services%20Pages/01.%20Headers/E%20Waste%20Recycling.png" alt="Card image cap" />
+            </div>
+            <Card.Body>
+              <Card.Title>Electronic waste and equipment recycling service</Card.Title>
+              <Card.Text>
+              It’s predicted that by 2030, global e-waste will reach 74 million metric tons - almost a doubling in just 16 years, making e-waste the world’s fastest-growing domestic waste stream. In 2019 alone, a record 53.6 million metric tons of electronic waste was generated worldwide.
+              </Card.Text>
+              <Button variant="outline-primary" href="https://www.tes-amm.com/it-services/e-waste-recycling/#!/">Read More</Button>
+            </Card.Body>
+          </Card>
+          <Card>
+            <div className="card-img-body">
+              <Card.Img src="https://www.nhm.ac.uk/content/dam/nhmwww/discover/minerals-mobile/ewaste-two-column.jpg.thumb.768.768.jpg" alt="Card image cap" />
+            </div>
+            <Card.Body>
+              <Card.Title>What is e-waste and what can we do about it?</Card.Title>
+              <Card.Text>
+              Prof Richard Herrington, Head of Earth Sciences at the Museum, shares some tips for how to dispose of unwanted electronics and tells us about an innovative urban mining project.
+              If you have a few redundant or broken electronics languishing in drawers at home, you're not alone. The UK is one of the largest producers of household e-waste in the world and we can all be part of the solution. 
+              </Card.Text>
+              <Button variant="outline-primary" href="https://www.nhm.ac.uk/discover/what-is-ewaste-and-what-can-we-do-about-it.html#:~:text=E%2Dwaste%20(electronic%20waste),Piles%20of%20mixed%20electronic%20waste."
+              >Read More</Button>
+            </Card.Body>
+          </Card>
+          <Card>
+            <div className="card-img-body">
+              <Card.Img src="https://www.unido.org/sites/default/files/2022-10/023%20-%2020220928%20UNIDO%2016x9.jpg" alt="Card image cap" />
+            </div>
+            <Card.Body>
+              <Card.Title>The Philippines: making money making e-waste safe</Card.Title>
+              <Card.Text>
+              MANILA - The United Nations’ most recent Global E-Waste Monitor report estimates that electronic waste generated worldwide will exceed 74 million metric tonnes by 2030.
+
+Higher consumption of electrical and electronic equipment, as well as short life cycles and limited repair options, will drive the increase.
+
+According to the report, the Philippines is among Southeast Asia's top e-waste generators. The country is estimated to have generated 3.9 kg of e-waste per capita in 2019. 
+              </Card.Text>
+              <Button variant="outline-primary" href="https://www.unido.org/news/philippines-making-money-making-e-waste-safe">Read More</Button>
+            </Card.Body>
+          </Card>
+        </div>
+      </Container>
+
+      <div class="container-fluid">
+      <div class="container-fluid mt-md-5">
+        <div class="row">
+          <div class="col-md-6 ml-md-5 home-paragraph">
+          <h1>Environmental Risks</h1>
+            <p>
+            E-waste can be toxic, is not biodegradable and accumulates in the environment, in the soil, air, water and living things. 
+            For example, open-air burning and acid baths being used to recover valuable materials from electronic components release toxic 
+            materials leaching into the environment. These practices can also expose workers to high levels of contaminants such as lead, mercury, 
+            beryllium, thallium, cadmium and arsenic, and also brominated flame retardants (BFRs) and polychlorinated biphenyls, which can lead to 
+            irreversible health effects, including cancers, miscarriages, neurological damage and diminished IQs.
+            </p>
+            <a href="https://www.genevaenvironmentnetwork.org/resources/updates/the-growing-environmental-risks-of-e-waste/">Read More</a>
+
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+    <div class="container-fluid">
+      <div class="container-fluid mt-md-5">
+        <div class="row">
+          <div class="col-md-6 ml-md-5 home-paragraph">
+          <h1>At 59 Million Tons, Our E-Waste Problem Is Getting Out of Control</h1>
+            <p>
+            With each passing year, our electronics—all the smartphones, laptops, monitors, TVs, speakers, and other personal devices that, 
+            for better or for worse, have become indispensable to modern life—get smaller, slimmer, and more lightweight. One of the earliest 
+            laptops, the Macintosh PowerBook 100, weighed more than five pounds when it was released in 1991; the lightest laptop currently on 
+            the market is less than two. The lightest phone you can find today weighs in at just over three ounces, less than a deck of playing cards. 
+            And as wireless technology continues to develop, we need fewer and fewer of the cords and cables that we once relied on for connectivity.
+            </p>
+            <a href="https://www.nrdc.org/stories/59-million-tons-our-e-waste-problem-getting-out-control">Read More</a>
+
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+    <div class="container-fluid">
+      <div class="container-fluid mt-md-5">
+        <div class="row">
+          <div class="col-md-6 ml-md-5 home-paragraph">
+            <h1>India's e-waste recycling "markets" are toxic nightmares filled with child laborers</h1>
+            <p>
+            India passed comprehensive e-waste recycling regulations in 2016, but they are not well enforced. Researchers from the UNSW report from Seelampur 
+            and Mandoli, two slums outside of Delhi, where the kabadiwalas (also called raddiwalas) work to dismantle truckloads of e-waste that are dumped there each day.
+            The workers of Seelampur and Mandoli, many of them children, lack even the most basic safety equipment: 
+            many work without goggles, masks, gloves, or work-shoes, and work with unsuitable and primitive tools to remove
+            precious metals and component from the guts of appliances, phones and other electronics.
+            </p>
+            <a href="https://boingboing.net/2019/02/15/three-dollars-a-day.html">Read More</a>
+
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+    </div>
+
+
+      
+  );
+};
+
+export default CardComponent;
